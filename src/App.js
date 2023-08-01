d3987f846f45dc30d68f3347ff3e8811dd65708f
Aaron Enyetu <aaronenyetu@gmail.com>
2023-08-01 11:28:35 -0700

    connected App to Alan AI studio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import alanBtn from '@alan-ai/alan-sdk-web'
+
+
+const alanKey = 'efc63fe002222be6d5cc6457ce709aba2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command }) => {
+                if (command === 'testCommand') {
+                    alert('This code was executed')
+                }
+            }
+        })
+    }, [])
+
+
     return (
         
         <div>
