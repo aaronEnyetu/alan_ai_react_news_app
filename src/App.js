b30358470fabd510c363efd5bd3f9855561beb65
Aaron Enyetu <aaronenyetu@gmail.com>
2023-08-01 22:35:22 -0700

    implemented logo and info containers. News by Terms functionality works

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import alanBtn from '@alan-ai/alan-sdk-web'
 import NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './styles';
 
 
 const alanKey = 'efc63fe002222be6d5cc6457ce709aba2e956eca572e1d8b807a3e2338fdd0dc/stage';
@@ -8,6 +9,9 @@ const alanKey = 'efc63fe002222be6d5cc6457ce709aba2e956eca572e1d8b807a3e2338fdd0d
 const App = () => {
 
     const [newsArticles, setNewsArticles] = useState([])
+
+
+    const classes = useStyles();
 
     useEffect(() => {
         alanBtn({
@@ -26,7 +30,9 @@ const App = () => {
     return (
         
         <div>
-            <h1>AI News Application</h1>
+            <div className={classes.logoContainer}>
+            <img src="https://www.intelligentcio.com/me/wp-content/uploads/sites/12/2019/04/AI-1000-web.jpg" className={classes.alanLogo} alt="logo" />
+              </div>
             <NewsCards articles={newsArticles} />
         </div>
     )
